5031b4a834bc75b9af567c372f06970497718024
GermanSC <germanscillone@gmail.com>
2015-12-16 17:31:23 -0300

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Pi_Searcher.c
+ Author      : German Sc.
+ Version     :
+ Copyright   : Todo sumamente copyrighteado 2015
+ Description : Busca cadenas numericas especificas en los 100.000.000
+ 	 	 	   primeros decimales de PI.
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Pi Searcher:\n");
+
+	return EXIT_SUCCESS;
+}
