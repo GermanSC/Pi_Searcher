ac05a5d1edef94e5953150cbf5a86793acd46b74
GermanSC <germanscillone@gmail.com>
2015-12-16 18:10:03 -0300

    Agregado printHelp

--- src/main.c
+++ src/main.c
@@ -12,9 +12,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void printHelp(char* Name)
+{
+	printf( "\nUso: %s [opciones] secuencia\n\n", Name);
+	printf( "  -N	Número de elementos previos y posteriores a la secuencia que se desean\n"
+			"	visualizar.\n"
+			"  -T	Número de hilos a utilizar para realizar la búsqueda.\n\n");
+	printf( "El programa busca cadenas numericas solicitadas como argumento en los "
+			"100.000.000 primeros numeros de pi. Devuele el tiempo transcurrido, tiempo "
+			"de busqueda y cantidad de apariciones de la secuencia, y posteriormete detalla "
+			"cada una de estas. Se puede configurar la cantidad de hilos a realizar la busqueda "
+			"y la cantidad de números a visualizar antes y despues de cada aparición.\n\n");
+
+}
+
 int main(int argc, char *argv[])
 {
 	printf("Pi Searcher:\n");
+	printHelp(argv[0]);
 
 	return EXIT_SUCCESS;
 }
