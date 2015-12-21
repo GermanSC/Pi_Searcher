d6da08511a6e5a8cd0c852f639cc6e6da3c18aa8
GermanSC <germanscillone@gmail.com>
2015-12-21 14:51:56 -0300

    Sub cadenas listo

--- src/main.c
+++ src/main.c
@@ -27,7 +27,6 @@
 char* lookforme = "1234";
 void* fm = NULL;
 unsigned int T = 4;
-unsigned int check_thread;
 
 /*	Semaforo	*/
 int sem_id;
@@ -35,7 +34,7 @@ struct sembuf sbuf;
 
 /*	Mutexs	*/
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t check = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t res_lock = PTHREAD_MUTEX_INITIALIZER;
 
 /*	FUNCIONES	*/
 
@@ -179,14 +178,6 @@ int lookFor(char * str, void* file_mem, unsigned int offset, unsigned int range)
 	unsigned int arg_len = strlen(str);
 	FILE* temp_file, *temp_result;
 
-	temp_result = fopen("/tmp/mitempres","w+b");
-	if(temp_result == NULL )
-	{
-		printf("Error al abrir el archivo temporal \"mitempres\".\n");
-		return -1;
-
-	}
-
 	while(arg_len > 0)
 	{
 		cnt = 0;
@@ -202,6 +193,11 @@ int lookFor(char * str, void* file_mem, unsigned int offset, unsigned int range)
 				{
 					pthread_mutex_lock(&lock);
 					temp_file = fopen("/tmp/mitemp","a");
+					if(temp_file == NULL )
+					{
+						printf("Error al abrir el archivo temporal \"mitemp\".\n");
+						return -1;
+					}
 
 					fprintf(temp_file,"%u ",i+offset);
 					fprintf(temp_file,"\n");
@@ -212,22 +208,31 @@ int lookFor(char * str, void* file_mem, unsigned int offset, unsigned int range)
 			}
 			i++;
 		}
-		//fprintf(temp_result,"%u\n",cnt);
+		pthread_mutex_lock(&res_lock);
+		temp_result = fopen("/tmp/mitempres","a");
+		if(temp_result == NULL )
+		{
+			printf("Error al abrir el archivo temporal \"mitempres\".\n");
+			return -1;
+		}
+
+		fprintf(temp_result,"%u %u\n",arg_len, cnt);
+		fclose(temp_result);
+
+		pthread_mutex_unlock(&res_lock);
 		arg_len--;
 	}
-	pthread_mutex_lock(&lock);
-	fclose(temp_result);
-	pthread_mutex_unlock(&lock);
+
 	return 0;
 }
 
 int printOccur(unsigned int nums, char * str, void* file_mem)
 {
-	int read;
+	int read, count, temp, count_ant;
 	unsigned int order = 0;
 	unsigned int pos;
 	FILE* tmp;
-//	unsigned int len = strlen(str);
+	unsigned int len = strlen(str);
 
 	tmp = fopen("/tmp/mitempres","r");
 
@@ -238,19 +243,30 @@ int printOccur(unsigned int nums, char * str, void* file_mem)
 		return -1;
 	}
 
-//	while(len > 0)
-//	{
-//		read = fscanf(tmp,"%u",&pos);
-//		printf("Apariciones de %*.*s: %u \n", (int)strlen(str), len, str, pos);
-//		len--;
-//	}
-//
-//
-//
-//	fclose(tmp);
-//	unlink("/tmp/mitempres");
-//
-//	printf("\n");
+	while(len > 0)
+	{
+		count_ant = 0;
+		read = 0;
+		while(read != EOF)
+		{
+			read = fscanf(tmp,"%u",&temp);
+			if(temp == len)
+			{
+				read = fscanf(tmp,"%u",&count);
+				count_ant += count;
+			} else {
+				read = fscanf(tmp,"%*u");
+			}
+		}
+		fseek(tmp,0,SEEK_SET);
+		printf("Apariciones de %*.*s: %u \n", (int)strlen(str), len, str, count_ant);
+		len--;
+	}
+	fclose(tmp);
+	unlink("/tmp/mitempres");
+
+	printf("\nPresione ENTER para ver las apariciones de %s",lookforme);
+	getchar();
 
 	tmp = fopen("/tmp/mitemps","r");
 	if(tmp == NULL )
@@ -272,6 +288,7 @@ int printOccur(unsigned int nums, char * str, void* file_mem)
 	}
 
 	fclose(tmp);
+	unlink("/tmp/mitemps");
 	printf("\n");
 	return 0;
 }
@@ -285,6 +302,7 @@ int sortResults(void)
 		printf("Error al abrir el archivo temporal.\n");
 		return -1;
 	}
+	fclose(tmp);
 	system("sort -n '/tmp/mitemp' > '/tmp/mitemps'");
 	unlink("/tmp/mitemp");
 	return 0;
@@ -302,8 +320,6 @@ void* threadFunc(void* parameter)
 		range = (unsigned int)(FILE_LENGTH/T);
 	}
 
-	printf("Intentando: string: %s range: %u | offset: %u\n", lookforme, range, c);
-
 	/*	Abro el semaforo	*/
 	sbuf.sem_op = 1;
 	semop(sem_id,&sbuf,1);
@@ -319,12 +335,15 @@ void* threadFunc(void* parameter)
 int cleanUp(void * file_mem)
 {
 	int tmp;
-	unlink("/tmp/mitemps");
+
+	unlink("/tmp/mitempres");
+
 	tmp = munmap (file_mem, FILE_LENGTH);
 	if (tmp == -1)
 	{
 		printf("Error al desmontar la memoria\n");
 	}
+
 	tmp = semctl(sem_id, 0, IPC_RMID);
 	if(tmp == -1)
 	{
@@ -368,11 +387,11 @@ int main(int argc, char *argv[])
 	signed	 int index		= 1;
 	unsigned int ctrl		= 0;
 	unsigned int N			= 10;
-//	unsigned int T			= 4;
 
 	/*	Variables de hilos	*/
 	static unsigned int thread_offset;
 	pthread_t threadid;
+	unsigned int thread_arg;
 
 	/*	Punteros	*/
 	//void* fm	= NULL;
@@ -419,8 +438,8 @@ int main(int argc, char *argv[])
 
 	for(ctrl = 0; ctrl < T; ctrl++ )
 	{
-		check_thread = (FILE_LENGTH / T) * ctrl;
-		pthread_create(&threadid, NULL, (void *)&threadFunc , &check_thread);
+		thread_arg = (FILE_LENGTH / T) * ctrl;
+		pthread_create(&threadid, NULL, (void *)&threadFunc , &thread_arg);
 		sbuf.sem_op = -1;
 		semop(sem_id,&sbuf,1);
 	}
@@ -439,8 +458,7 @@ int main(int argc, char *argv[])
 	ctrl = sortResults();
 	ctrl = printOccur(N, argv[index], fm);
 
-	printf("termino!\n");
-	getchar();
+	printf("Termino!\n");
 
 	/*----	Fin de Programa		----*/
 	ctrl = cleanUp(fm);
